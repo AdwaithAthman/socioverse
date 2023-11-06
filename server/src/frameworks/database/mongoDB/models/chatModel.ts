fcfb2d8da141125001886b706ac396f5d7f32207
AdwaithAthman <iamadwaith17@gmail.com>
2023-11-06 10:57:16 +0530

    Created chatModel.ts

---
+++ server/src/frameworks/database/mongoDB/models/chatModel.ts
@@ -0,0 +1,40 @@
+import { Document, Schema, model } from "mongoose";
+
+interface ChatInterface extends Document {
+  chatName: string;
+  isGroupChat: boolean;
+  users: Schema.Types.ObjectId[];
+  latestMessage: Schema.Types.ObjectId;
+  groupAdmin: Schema.Types.ObjectId;
+}
+
+const chatSchema = new Schema<ChatInterface>(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Chat = model<ChatInterface>("Chat", chatSchema);
+export default Chat;

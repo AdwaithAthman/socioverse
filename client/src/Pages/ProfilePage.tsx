dee859854910a0834ba43d3d5e30df32a079265e
AdwaithAthman <iamadwaith17@gmail.com>
2023-12-11 14:00:58 +0530

    Removed unused code from profile page

--- client/src/Pages/ProfilePage.tsx
+++ client/src/Pages/ProfilePage.tsx
@@ -1,8 +1,4 @@
-// import { useEffect } from "react";
 import Profile from "../Components/Profile";
-// import { useNavigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { StoreType } from "../Redux/Store";
 
 const ProfilePage = () => {
   return (

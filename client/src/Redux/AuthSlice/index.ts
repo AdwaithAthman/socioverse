15dd3dbdc0eb49dd6167970d8ab151c287706ba6
AdwaithAthman <iamadwaith17@gmail.com>
2023-12-11 14:04:44 +0530

    Removed unused code fromauth slice

--- client/src/Redux/AuthSlice/index.ts
+++ client/src/Redux/AuthSlice/index.ts
@@ -1,11 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { loginUser, loginUsingGoogle, refreshAccessToken } from "../../API/Auth";
-// import END_POINTS from "../../Constants/endpoints";
 
 //types
 import { User } from "../../Types/loginUser";
-// import { LoginUserInterface } from "../../Types/loginUser";
-// import { GoogleLoginInterface } from "../../Types/loginUser";
 
 interface AuthState {
     user: User | null; 
@@ -49,37 +45,4 @@ const authSlice = createSlice({
 export const { setCredentials, logout, removeFollower, addFollower } = authSlice.actions;
 export default authSlice.reducer;
 
-
-// Async thunks
-// export const login = createAsyncThunk(
-//     END_POINTS.LOGIN_USER,
-//     async (credentials: LoginUserInterface) => {
-//       const response = await loginUser(credentials);
-//       return {
-//         accessToken: response.accessToken,
-//         refreshToken: response.refreshToken  
-//       };
-//     }
-//   );
-
-//   export const loginWithGoogle = createAsyncThunk(
-//     END_POINTS.LOGIN_GOOGLE,
-//     async (credentials: GoogleLoginInterface ) => {
-//       const response = await loginUsingGoogle(credentials);
-//       return {
-//         accessToken: response.accessToken,
-//         refreshToken: response.refreshToken
-//       };
-//     }  
-//   );
-
-//   export const refresh = createAsyncThunk(
-//     END_POINTS.REFRESH_TOKEN,
-//     async (refreshToken: string) => {
-//       const accessToken = await refreshAccessToken(refreshToken);
-//       return { accessToken }; 
-//     }
-//   );
-  
-  
 

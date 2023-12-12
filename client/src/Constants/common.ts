4a6d1c2492307ab02bbc26066475dfd2291691fd
AdwaithAthman <iamadwaith17@gmail.com>
2023-12-12 17:30:03 +0530

    Added svg links

--- client/src/Constants/common.ts
+++ client/src/Constants/common.ts
@@ -4,6 +4,16 @@ const CONSTANTS_COMMON = {
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   CLIENT_BASE_URL: import.meta.env.VITE_CLIENT_BASE_URL,
   DEFAULT_IMG: import.meta.env.VITE_DEFAULT_IMG,
+  ADMIN_LOGIN_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381015/socioverse%20stock/AdminLoginSvg_ain9jo.svg",
+  AUTHENTICATION_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381022/socioverse%20stock/AuthenticationSvg_iirmb2.svg",
+  CHAT_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381151/socioverse%20stock/ChatSvg_acpykg.svg",
+  GOOGLE_LOGO_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381161/socioverse%20stock/GoogleLogoSvg_atvkrt.svg",
+  LOADER_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381169/socioverse%20stock/Loader_dofs8c.svg",
+  LOGIN_ALERT_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381181/socioverse%20stock/LoginAlertSvg_yx47iz.svg",
+  NO_DATA_AVAILABLE_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381193/socioverse%20stock/NoDataAvailable_jovfkq.svg",
+  PROFILE_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381205/socioverse%20stock/profileSvg_mwpb71.svg",
+  SETTING_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381215/socioverse%20stock/Settings_lv6rse.svg",
+  ERROR_NOT_FOUND_SVG: "https://res.cloudinary.com/dkxyfsxso/image/upload/v1702381003/socioverse%20stock/404_nvavnv.svg"
 };
 
 export const TOAST_ACTION: ToastOptions = {
